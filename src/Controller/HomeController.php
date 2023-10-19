96fa9d0ca4164bad7371b98d27403c2cffbb0ad8
Karma_Ordi <karmalinkspro66@gmail.com>
2023-10-19 14:26:49 +0400

    Afficher le formulaire pour créer un poste et l'enregistrer dans la db

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
+        $user = $this->getUser();
+
+        if ($user) {
+            $post = new Post();
+            $form = $this->createForm(PostType::class, $post);
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $post->setUser($user);
+                $em->persist($post);
+                $em->flush();
+            }
+            return $this->render('home/index.html.twig', [
+                'form' => $form
+            ]);
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
